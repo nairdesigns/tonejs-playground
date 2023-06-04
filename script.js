e52b6725aed7063b9462d68a7bfee9bf89ae7c9c
Gonzales <brian.gonzales@religionnews.com>
2023-06-05 00:31:22 +0900

    created effects

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 function playMelody() {
   // Create a Tone.js polyphonic synth
-  const synth = new Tone.Synth({
-    oscillator: {
-      type: "sine", // Use a sine wave for a smooth sound
-    },
-    envelope: {
-      attack: 0.1,
-      decay: 0.2,
-      sustain: 0.5,
-      release: 1,
-      
-    },
-  }).toDestination();
-  // Define the sad melody with notes and durations
+  const synth = createSynth();
+
+  // Define the melody with notes and durations
   const melody = [
     { note: "A4", duration: "4n" },
     { note: "G4", duration: "4n" },
@@ -24,21 +14,48 @@ function playMelody() {
     { note: "E4", duration: "2n" },
   ];
 
-  const reverb = new Tone.Reverb({
+  // Create and connect effects
+  const reverb = createReverb();
+  const chorus = createChorus();
+  connectEffects(synth, chorus, reverb);
+
+  // Play the melody
+  playNoteSequence(synth, melody);
+}
+
+function createSynth() {
+  return new Tone.Synth({
+    oscillator: {
+      type: "sine", // Use a sine wave for a smooth sound
+    },
+    envelope: {
+      attack: 0.1,
+      decay: 0.2,
+      sustain: 0.5,
+      release: 1,
+    },
+  }).toDestination();
+}
+
+function createReverb() {
+  return new Tone.Reverb({
     decay: 5, // Adjust the decay time for the reverb effect
   });
+}
 
-  // Create a chorus effect
-  const chorus = new Tone.Chorus({
+function createChorus() {
+  return new Tone.Chorus({
     frequency: 1.5, // Adjust the chorus frequency
     depth: 0.5, // Adjust the chorus depth
     delayTime: 3, // Adjust the chorus delay time
   });
+}
 
-  // Connect the synth to the effects and then to the destination
+function connectEffects(synth, chorus, reverb) {
   synth.chain(chorus, reverb, Tone.Destination);
+}
 
-  // Play the melody
+function playNoteSequence(synth, melody) {
   let index = 0;
   const playNote = () => {
     if (index >= melody.length) return;
