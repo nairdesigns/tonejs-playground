9e0d849762ddddc9fc5864f4d23a26ce39c3304f
Gonzales <brian.gonzales@religionnews.com>
2023-06-05 14:24:20 +0900

    removed module

--- app.js
+++ app.js
@@ -3,21 +3,43 @@ import { createSynth } from "./synth";
 import { createReverb, createChorus, connectEffects } from "./effects";
 import { playNoteSequence } from "./melody.js";
 
-function playMelody(reverbDecayInput) {
-  const synth = createSynth();
-  const chorus = createChorus();
-  const reverb = createReverb();
 
-  connectEffects(synth, chorus, reverb);
 
-  console.log(reverbDecayInput);
-  const melody = [
-    // Define the melody...
-  ];
-
-  playNoteSequence(synth, melody);
-  console.log(reverb.decay);
-
-  if (reverbDecayInput) {
-    reverbDecayInput.addEventListener
-  }}
+console.log('app.js loaded!')
+export function playMelody(reverbDecayInput) {
+    const synth = createSynth();
+    const chorus = createChorus();
+    const reverb = createReverb();
+    
+    
+    document.getElementById("playButton").addEventListener("click", function () {
+        const reverbDecayInput = document.getElementById("reverbDecay");
+        playMelody(reverbDecayInput);
+    });
+    
+    connectEffects(synth, chorus, reverb);
+    
+    console.log(reverbDecayInput);
+    const melody = [
+        { note: "A4", duration: "4n" },
+        { note: "G4", duration: "4n" },
+        { note: "E4", duration: "2n" },
+        { note: "C4", duration: "4n" },
+        { note: "D4", duration: "4n" },
+        { note: "E4", duration: "4n" },
+        { note: "F4", duration: "4n" },
+        { note: "E4", duration: "2n" },
+    ];
+    
+    playNoteSequence(synth, melody);
+    console.log(reverb.decay);
+    
+    if (reverbDecayInput) {
+        reverbDecayInput.addEventListener("input", () => {
+            const decayValue = parseFloat(reverbDecayInput.value);
+            reverb.decay = decayValue;
+            console.log("reverb working");
+            console.log(reverb.decay);
+        });
+    }
+}
